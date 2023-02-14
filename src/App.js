255255720e1234ad615803ba75b8d144165db2e2
Roger Earl <rogerearl@me.com>
2023-02-13 21:32:22 -0800

    finished suspense

--- src/App.js
+++ src/App.js
@@ -1,60 +1,48 @@
-import { Fragment, useEffect, useState, useRef } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useEffect, useState, Suspense } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
-import { SearchContext } from './context/SearchContext'
-import AlbumView from './components/AlbumView'
-import ArtistView from './components/ArtistView'
-import { DataContext } from './context/DataContext'
+import { createResource as fetchData } from './helper'
+
 
 
 function App() {
 	let [search, setSearch] = useState('')
 	let [message, setMessage] = useState('Search for Music!')
-	let [data, setData] = useState([])
+	let [data, setData] = useState(null)
 
-	const API_URL = 'http://localhost:4000/music/'
 
 	useEffect(() => {
-		if(search) {
-			const fetchData = async () => {
-				document.title = `${search} Music`
-				const response = await fetch(API_URL + search)
-				const resData = await response.json()
-				if (resData.results.length > 0) {
-					setData(resData.results)
-				} else {
-					setMessage('Not Found')
-				}
-			}
-			fetchData()
+		if (search) {
+			setData(fetchData(search))
 		}
 	}, [search])
+	
 	
 	const handleSearch = (e, term) => {
 		e.preventDefault()
 		setSearch(term)
 	}
 
+	const renderGallery = () => {
+		if(data){
+			return (
+				<Suspense fallback={<h1>Loading...</h1>} >
+					<Gallery data={data} />
+				</Suspense>
+			)
+		}
+	}
+	
+
 	return (
-		<div>
+		<div className="App">
+			<SearchBar handleSearch={handleSearch} />
 			{message}
-			<Router>
-				<Routes>
-					<Route path="/" element={
-						<Fragment>
-							<SearchBar handleSearch = {handleSearch}/>
-							<DataContext.Provider value={data}>
-								<Gallery />
-							</DataContext.Provider>
-						</Fragment>
-					} />
-					<Route path="/album/:id" element={<AlbumView />} />
-					<Route path="/artist/:id" element={<ArtistView />} />
-				</Routes>
-			</Router>
+			{renderGallery()}
 		</div>
-  	);
+	)
+	
+	
 }
 
 export default App;
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,19 +2,21 @@ import { useContext } from 'react'
 import { DataContext } from '../context/DataContext'
 import GalleryItem from './GalleryItem'
 
-function Gallery(){
-    const data = useContext(DataContext)
-    
-    const display = data.map((item,index) => {
+const Gallery = (props) => {
+    const data = props.data.result.read()
+
+    const display = data.map((item, index) => {
         return (
             <GalleryItem item={item} key={index} />
         )
     })
+
     return (
         <div>
             {display}
         </div>
     )
 }
+
 
 export default Gallery
